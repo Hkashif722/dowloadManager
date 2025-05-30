40f9edb25e2a0e8c0ab1fdb34601265f163b60fa
Kashif Hussain <kashif.hussain@enthral.ai>
2025-05-30 19:13:40 +0530

    remove codable conformance

--- Sources/DownloadManager/Core/Protocols/DownloadableItem.swift
+++ Sources/DownloadManager/Core/Protocols/DownloadableItem.swift
@@ -10,7 +10,7 @@
 import Foundation
 
 /// Represents an individual downloadable item (e.g., Module)
-public protocol DownloadableItem: AnyObject, Identifiable, Codable, Sendable {
+public protocol DownloadableItem: AnyObject, Identifiable, Sendable {
     associatedtype DownloadType: DownloadTypeProtocol
     
     var id: UUID { get }
--- Sources/DownloadManager/Core/Protocols/DownloadableModel.swift
+++ Sources/DownloadManager/Core/Protocols/DownloadableModel.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 /// Represents a container model (e.g., Course) that contains downloadable items
-public protocol DownloadableModel: AnyObject, Identifiable, Codable, Sendable {
+public protocol DownloadableModel: AnyObject, Identifiable, Sendable {
     associatedtype ItemType: DownloadableItem
     
     var id: UUID { get }
