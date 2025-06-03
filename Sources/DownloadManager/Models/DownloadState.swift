ac20cb56cc30c554696cd187b584369df7c48e1d
Kashif Hussain <57903864+Hkashif722@users.noreply.github.com>
2025-06-03 19:15:26 +0530

    Update DownloadState.swift

--- Sources/DownloadManager/Models/DownloadState.swift
+++ Sources/DownloadManager/Models/DownloadState.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// Represents the various states of a download operation
-public enum DownloadState: String, CaseIterable, Codable, Equatable, Sendable {
+public enum DownloadState: String, Codable, CaseIterable, Equatable, Sendable {
     case notDownloaded
     case queued
     case downloading
