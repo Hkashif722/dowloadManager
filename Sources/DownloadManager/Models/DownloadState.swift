3af5e3d395911353c99e55af86d8827c62750bc2
Kashif Hussain <57903864+Hkashif722@users.noreply.github.com>
2025-06-03 18:50:30 +0530

    Update DownloadState.swift

--- Sources/DownloadManager/Models/DownloadState.swift
+++ Sources/DownloadManager/Models/DownloadState.swift
@@ -1,13 +1,37 @@
-// MARK: - Download State
 import Foundation
 
-// MARK: - Fixed Download State (Sendable)
-public enum DownloadState: String, Codable, Equatable, Sendable {
+/// Represents the various states of a download operation
+enum DownloadState: CaseIterable {
     case notDownloaded
     case queued
     case downloading
     case paused
     case downloaded
     case failed
-    case cancelling // Intermediate state for cancellation
+    case cancelling
+    
+    /// Whether the download is currently in progress
+    var isInProgress: Bool {
+        [.downloading, .queued].contains(self)
+    }
+    
+    /// Whether the download can be started or resumed
+    var canStart: Bool {
+        [.notDownloaded, .failed].contains(self)
+    }
+    
+    /// Whether the download can be paused
+    var canPause: Bool {
+        self == .downloading
+    }
+    
+    /// Whether the download can be resumed
+    var canResume: Bool {
+        self == .paused
+    }
+    
+    /// Whether the content is available for playback
+    var isPlayable: Bool {
+        self == .downloaded
+    }
 }
